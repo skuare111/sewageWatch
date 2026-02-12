810992ed3ef434922a025f393450b56c2b5d227b
sakura_ttt <2990366617@qq.com>
2026-02-12 11:23:24 +0800

    Add ComponentScan annotation to SewageWatchApplication

--- sewage-watch-JAVA/src/main/java/com/zhuchen/SewageWatchApplication.java
+++ sewage-watch-JAVA/src/main/java/com/zhuchen/SewageWatchApplication.java
@@ -3,9 +3,10 @@ package com.zhuchen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.zhuchen", "com.zhuchen.controller", "com.zhuchen.service"})
 public class SewageWatchApplication {
 
     public static void main(String[] args) {
